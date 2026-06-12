c0d93a2bc0ece55aefa771d58aa0b4abae129730
kirubhel <kirub.hel@gmail.com>
2026-06-12 09:59:10 +0300

    refactor: migrate footer-links to .tsx and replace Twitter icon with custom X logo

--- src/lib/footer-links.tsx
+++ src/lib/footer-links.tsx
@@ -1,4 +1,23 @@
-import { Facebook, Globe, Instagram, Linkedin, Mail, MessageCircle, Twitter, Youtube, Video, Send, Images} from "lucide-react";
+import { Facebook, Globe, Instagram, Linkedin, Mail, MessageCircle, Youtube, Video, Send, Images} from "lucide-react";
+
+const TwitterX = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+    <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+  </svg>
+);
+
 
 export type FooterSocialIcon =
   | "facebook"
@@ -26,7 +45,7 @@ export interface FooterRouteLink {
 
 export const footerSocialIconMap = {
   facebook: Facebook,
-  twitter: Twitter,
+  twitter: TwitterX,
   instagram: Instagram,
   linkedin: Linkedin,
   youtube: Youtube,
